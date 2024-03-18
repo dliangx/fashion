c5c06485c217d0d61ef151357d1410ba1786810d
dliang <humga@icloud.com>
2024-03-18 21:07:26 +0800

    remove unuse

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,22 @@
 use poem::{
-    error::BadRequest,
-    get, handler,
-    middleware::AddData,
-    post,
-    web::{Data, Json, Path},
-    EndpointExt, Result, Route,
+    get, middleware::AddData,
+    EndpointExt, Route,
 };
-use serde::{Deserialize, Serialize};
+
 use shuttle_poem::ShuttlePoem;
-use shuttle_runtime::CustomError;
-use sqlx::{Executor, FromRow, PgPool};
+use sqlx::PgPool;
 mod api;
 mod auth;
 
-
-#[handler]
-async fn retrieve(Path(id): Path<i32>, state: Data<&PgPool>) -> Result<Json<Todo>> {
-    let todo = sqlx::query_as("SELECT * FROM todos WHERE id = $1")
-        .bind(id)
-        .fetch_one(state.0)
-        .await
-        
-        .map_err(BadRequest)?;
-
-    Ok(Json(todo))
-}
-
-#[handler]
-async fn add(Json(data): Json<TodoNew>, state: Data<&PgPool>) -> Result<Json<Todo>> {
-    let todo = sqlx::query_as("INSERT INTO todos(note) VALUES ($1) RETURNING id, note")
-        .bind(&data.note)
-        .fetch_one(state.0)
-        .await
-        .map_err(BadRequest)?;
-
-    Ok(Json(todo))
-}
-
 #[shuttle_runtime::main]
 async fn poem(#[shuttle_shared_db::Postgres] pool: PgPool) -> ShuttlePoem<impl poem::Endpoint> {
-
     // pool.execute(include_str!("../schema.sql"))
     //     .await
     //     .map_err(CustomError::new)?;
 
     let app = Route::new()
-        .at("/todo", post(add))
-        .at("/todo/:id", get(retrieve))
+        .at("/list_blog", get(api::content::list_blog))
+        .at("/blog/:id", get(api::content::list_blog))
         .with(AddData::new(pool));
     Ok(app.into())
-}
-
-#[derive(Deserialize)]
-struct TodoNew {
-    pub note: String,
-}
-
-#[derive(Serialize, FromRow)]
-struct Todo {
-    pub id: i32,
-    pub note: String,
 }
