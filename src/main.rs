2c4c078522f70573d055db17bdd8d0845b02db5e
dliang <humga@icloud.com>
2024-04-16 20:31:54 +0800

    cors config

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use poem::{get, handler, middleware::AddData, post, EndpointExt, Route};
+use poem::{
+    get, handler,
+    http::Method,
+    middleware::{AddData, Cors},
+    post, EndpointExt, Route,
+};
 use shuttle_poem::ShuttlePoem;
 use shuttle_runtime::CustomError;
 use sqlx::{Executor, PgPool};
@@ -11,9 +16,14 @@ async fn hello() -> String {
 }
 #[shuttle_runtime::main]
 async fn poem(#[shuttle_shared_db::Postgres] pool: PgPool) -> ShuttlePoem<impl poem::Endpoint> {
-    pool.execute(include_str!("../sql/schema.sql"))
-        .await
-        .map_err(CustomError::new)?;
+    // pool.execute(include_str!("../sql/schema.sql"))
+    //     .await
+    //     .map_err(CustomError::new)?;
+    let cors = Cors::default()
+        .allow_origin("http://localhost:1420")
+        .allow_method(Method::GET)
+        .allow_method(Method::POST)
+        .allow_credentials(true);
 
     let api = Route::new()
         .at("/recommend", post(api::home::home_recommend))
@@ -50,6 +60,7 @@ async fn poem(#[shuttle_shared_db::Postgres] pool: PgPool) -> ShuttlePoem<impl p
         .at("/login", post(auth::login))
         .at("/register", post(auth::register))
         .nest("/api", api)
+        .with(cors)
         .with(AddData::new(pool));
     Ok(app.into())
 }
